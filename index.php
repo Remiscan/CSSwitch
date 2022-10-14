d0b311118388dd2607f3eed6db21e1c00457c3cc
Rémi Scandella <remi.scandella@gmail.com>
2022-10-14 15:23:22 +0200

    Use Translation.php

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
 $commonDir = '../_common';
 require_once $commonDir.'/php/version.php';
-require_once $commonDir.'/php/httpLanguage.php';
-require_once $commonDir.'/php/getStrings.php';
+require_once $commonDir.'/php/Translation.php';
 
-$lang = httpLanguage() ?? 'en';
-$Textes = new Textes('csswitch', $lang);
+$translation = new Translation(__DIR__.'/strings.json');
+$httpLanguage = $translation->getLanguage();
 ?>
 <!doctype html>
-<html data-version="<?=version(__DIR__)?>" data-http-lang="<?=httpLanguage()?>">
+<html data-version="<?=version(__DIR__)?>" data-http-lang="<?=$httpLanguage?>">
   <head>
     <meta charset="utf-8">
-    <title>CSSwitch - <?=$Textes->getString('titre-page-description')?></title>
+    <title>CSSwitch - <?=$translation->get('titre-page-description')?></title>
     
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="initial-scale=1">
     <meta name="theme-color" content="#FFFFFF">
 
     <meta property="og:title" content="CSSwitch">
-    <meta name="description" content="<?=$Textes->getString('description-site')?>">
-    <meta property="og:description" content="<?=$Textes->getString('description-site')?>">
+    <meta name="description" content="<?=$translation->get('description-site')?>">
+    <meta property="og:description" content="<?=$translation->get('description-site')?>">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://remiscan.fr/csswitch/">
     <meta property="og:image" content="https://remiscan.fr/mon-portfolio/projets/csswitch/og-preview.png">
@@ -42,6 +41,8 @@ $Textes = new Textes('csswitch', $lang);
     <link rel="stylesheet" href="page.css">
 
     <!-- Import map -->
+    <script defer src="/_common/polyfills/adoptedStyleSheets.min.js"></script>
+    <script>window.esmsInitOptions = { polyfillEnable: ['css-modules', 'json-modules'] }</script>
     <script defer src="/_common/polyfills/es-module-shims.js"></script>
     <script type="importmap"><?php include 'importMap.json'; ?></script>
 
